6fdd6b4dde55ba169eb59ec3822ee35a3bac574b
rajy philip <rajy.p@xetgo.co>
2024-03-23 15:38:34 +0530

    adding helper classes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from "./logo.svg";
 import "./App.scss";
+import "./helper_classes.scss";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 import Playlist from "./components/Playlist/Playlist";
 import VideoDetails from "./components/VideoDetails/VideoDetails";
 
 function App() {
   return (
-    <div className="main-page">
-      <div className="main-page-content">
-        <div className="main-page-upper-container">
+    <div className="main-page ">
+      <div className="main-page-content border-box p-32">
+        <div className="main-page-upper-container flex-row gap-16">
           <VideoPlayer />
           <Playlist />
         </div>
